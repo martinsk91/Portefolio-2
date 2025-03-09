7d5fee61369eb4255a4bd9f249174b2ecbf12b40
martinsk91 <132287423+martinsk91@users.noreply.github.com>
2025-03-09 22:27:57 +0100

    updated projects

--- src/variabels/projects.js
+++ src/variabels/projects.js
@@ -1,26 +1,28 @@
 import sm2 from "../images/SM2.jpg"
 import ca from "../images/CA.jpg"
+import pe2 from "../images/pe2.jpg"
 
 const projects = [
     {
       name: "Semester Project 2",
-      description: "This is a description of project 1",
+      description: "Semester Project 2(Auction House) is a web application built with Bootstrap and Vanilla JavaScript. It is an auction platform where users can log in, create profiles, and participate in auctions. Users can place bids on items, and each auction has a countdown timer until it ends.",
       imageUrl: sm2,  
       repoUrl: "https://github.com/martinsk91/semester-project-2",
       liveUrl: "https://martin-s-semester-project-2.netlify.app/",
     },
     {
-      name: "Cross course assignment",
-      description: "This is a description of project 2",
+      name: "Javascript frameworks assignment",
+      description: "This project is an online store built with React and styled with Bootstrap. Users can browse products, add items to their cart, and proceed to checkout. It offers a seamless shopping experience with product search and responsive design.",
       imageUrl: ca,
       repoUrl: "https://github.com/martinsk91/react-ca", 
       liveUrl: "https://react-ca-martin.netlify.app/",
     },
     {
-      name: "Project 3",
-      description: "This is a description of project 3",
-      imageUrl: "/assets/project3.jpg", 
-      repoUrl: "https://github.com/your-username/project3", 
+      name: "project exam 2",
+      description: "Project Exam 2 is a web application focused on booking management. It allows users to create and book available slots. The project is still in progress and has not been fully completed yet.",
+      imageUrl: pe2, 
+      repoUrl: "https://github.com/martinsk91/project-exam-2",
+      liveUrl: "https://project-exam-2-martin-s.netlify.app/" 
     },
   ];
 
