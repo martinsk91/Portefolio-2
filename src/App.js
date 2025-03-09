fa33fbdce515a0ab8bfcb21fe36522b64cc470c1
martinsk91 <132287423+martinsk91@users.noreply.github.com>
2025-03-09 23:00:59 +0100

    removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from "react";
-import ProjectCard from "./components/ProjectCard.js";
-import projects from "./variabels/projects.js";
 import Header from "./components/Header.js";
 import About from "./components/About.js";
 import Projects from "./components/Projects.js";
